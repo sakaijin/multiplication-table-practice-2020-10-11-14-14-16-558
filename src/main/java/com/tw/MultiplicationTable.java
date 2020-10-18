bfac311cf8014cd24c0cd6812d992a59c75e6828
Erik <erik.daniel.nobleta@oocl.com>
2020-10-18 11:39:09 +0800

    [MultiplicationTable][Erik-1]feature: added conditions to pass all test cases

--- src/main/java/com/tw/MultiplicationTable.java
+++ src/main/java/com/tw/MultiplicationTable.java
@@ -2,6 +2,40 @@ package com.tw;
 
 public class MultiplicationTable {
     public String create(int startNumber, int endNumber) {
-        return null;
+        return generateMultiplicationTable(startNumber,endNumber);
+    }
+
+    private static String generateMultiplicationTable(int startInt, int endInt) {
+        if (!compareValueOfstartIntToendInt(startInt, endInt)){
+            return null;
+        }
+        if (!verifiyAllValues(startInt, endInt)){
+            return null;
+        }
+        return createMultiplicationTable(startInt,endInt);
+    }
+
+    private static String createMultiplicationTable(int startInt, int endInt) {
+        StringBuilder result = new StringBuilder();
+        for(int i = startInt;  i <= endInt; i++){
+            for(int j =  startInt; j <= i;j++){
+                result.append(j).append("*").append(i).append("=").append(i*j);
+                if (j != i){
+                    result.append("  ");
+                }
+            }
+            if(i <endInt){
+                result.append("\r\n");
+            }
+        }
+        return result.toString();
+    }
+
+    private static boolean verifiyAllValues(int startInt, int endInt) {
+        return startInt > 0 && startInt < 1000 && endInt > 0 && endInt < 1000 ;
+    }
+
+    private static boolean compareValueOfstartIntToendInt(int startInt, int endInt) {
+        return startInt <= endInt;
     }
 }
